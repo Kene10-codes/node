021c0221399e13ca9428cfdcacf5f41bc5bbfb50
keneNwobodo <nwobodokenechukwu2@gmail.com>
2022-08-04 21:36:06 +0100

    learnt how to send email using nodemailer

---
+++ email.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer")
+
+// set up the transporter
+let transporter = nodemailer.createTransport({
+	service: 'gmail',
+	port: 587,
+	secure: false,
+	auth: {
+		user: "test01@gmail.com",
+		pass: "##########"
+	}
+})
+
+let mailOptions = {
+	from: "test01@gmail.com",
+	to: "test02@gmail.com", "test04@gmail.com",
+	subject: "Sending email fron Node.js",
+    text: "That was easy!"
+}
+
+// send email
+transporter.sendMail(mailOptions, function(err, info){
+	if(err) {
+		console.log(err)
+	} else {
+		console.log("Email sent: " + info.response)
+	}
+
+})
